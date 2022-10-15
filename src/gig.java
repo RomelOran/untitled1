8cb35d9c76eb35dadc3e382dbcc8861bf856b6e5
romelorange@163.com <romelorange@163.com>
2022-10-15 18:13:03 +0800

    1

--- src/gig.java
+++ src/gig.java
@@ -3,4 +3,8 @@
  * @date 2022/10/15 - 17:15
  */
 public class gig {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("4241");
+    }
 }
---
+++ src/hghd.java
@@ -0,0 +1,6 @@
+/**
+ * @author Romel
+ * @date 2022/10/15 - 18:11
+ */
+public class hghd {
+}
